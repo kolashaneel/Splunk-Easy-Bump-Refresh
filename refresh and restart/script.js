2d231fc51ba4348e48e2b6344c639a5a7103d669
Shaneel <72293462+kolashaneel@users.noreply.github.com>
2023-10-08 21:11:31 +0530

    changed logic

--- refresh and restart/script.js
+++ refresh and restart/script.js
@@ -8,25 +8,35 @@ function getCurrentTabUrl(callback) {
 
 // Function to update the popup with the truncated URL
 function updatePopup(url) {
-  const splitby1 = "/app/"
-  const splitby2 = "/manager/"
-  splunk_url = url.split(splitby1)
-  if (splunk_url.length == 1) {
-    splunk_url = url.split(splitby2)
-    if (splunk_url.length == 1) {
-      document.getElementById("info").innerHTML = "Not a Splunk URL"
-    }
-    else {
-      document.getElementById('bump').addEventListener('click', () => {
-        window.open(splunk_url[0] + "/_bump")
-      });
-      document.getElementById('refresh').addEventListener('click', () => {
-        window.open(splunk_url[0] + "/debug/refresh")
-      });
+
+  const splitby_keyword = ["/app/", "/manager/"]    // splunk url will be identified using these words
+  let splitby = ""                                  // based on the url keyword will be assigned to this for extracting url
+
+  for (i = 0; i < splitby_keyword.length; i++) {    // this loop check the length of the arrary which contains the url split parts, if the length is greater than 1 that keyword will be assigned to splitby and breaks the loop
+    if (url.split(splitby_keyword[i]).length > 1) {
+      splitby = splitby_keyword[i]
+      break;
     }
   }
-}
 
+  if (splitby == "") {                              // if url does not contain any key word listed above 1st item from array will be assigned for further processing
+    splitby = splitby_keyword[0]
+  }
+
+  splunk_url = url.split(splitby)                   // check the split parts of the url
+
+  if (splunk_url.length == 1) {                     // check the length of the split array id length is 1 means it's not a splunk URL if length is greater than 1 bump and server refresh buttons will work
+    document.getElementById("info").innerHTML = "Not a Splunk URL"
+  }
+  else {
+    document.getElementById('bump').addEventListener('click', () => {
+      window.open(splunk_url[0] + "/_bump")
+    });
+    document.getElementById('refresh').addEventListener('click', () => {
+      window.open(splunk_url[0] + "/debug/refresh")
+    });
+  }
+}
 
 // Fetch the current tab's URL and update the popup
 getCurrentTabUrl(updatePopup);
