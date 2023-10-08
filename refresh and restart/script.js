586a1e8ec5c65c5b120f1bb01110c3700b95700f
Shaneel <72293462+kolashaneel@users.noreply.github.com>
2023-10-08 18:30:39 +0530

    changed log to split by /app/ or /manager/

--- refresh and restart/script.js
+++ refresh and restart/script.js
@@ -8,20 +8,25 @@ function getCurrentTabUrl(callback) {
 
 // Function to update the popup with the truncated URL
 function updatePopup(url) {
-  const regex_pattern=/\w+:\/\/\w+:\w+\/\w+-\w+\//;
-  const match = regex_pattern.exec(url);
-  if (match == null){
-    document.getElementById("info").innerHTML = "Not a Splunk URL"
-  }
-  else{
-    document.getElementById('bump').addEventListener('click', () => {
-      window.open(match[0]+"_bump")
-    });
-    document.getElementById('refresh').addEventListener('click', () => {
-      window.open(match[0]+"debug/refresh")
-    });
+  const splitby1 = "/app/"
+  const splitby2 = "/manager/"
+  splunk_url = url.split(splitby1)
+  if (splunk_url.length == 1) {
+    splunk_url = url.split(splitby2)
+    if (splunk_url.length == 1) {
+      document.getElementById("info").innerHTML = "Not a Splunk URL"
+    }
+    else {
+      document.getElementById('bump').addEventListener('click', () => {
+        window.open(splunk_url[0] + "/_bump")
+      });
+      document.getElementById('refresh').addEventListener('click', () => {
+        window.open(splunk_url[0] + "/debug/refresh")
+      });
+    }
   }
 }
+
 
 // Fetch the current tab's URL and update the popup
 getCurrentTabUrl(updatePopup);
